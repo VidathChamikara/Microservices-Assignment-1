f4e7a1fa1b51d271d2601b596ffdc42ee0f38b16
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-08-19 20:42:13 +0530

    establish the connection between mssql  datanbase

--- Node/Microservice_B_Order/app.js
+++ Node/Microservice_B_Order/app.js
@@ -1,9 +1,26 @@
 const express = require('express');
-
+const sql = require('mssql/msnodesqlv8');
 
 const app = express();
 
 const PORT = 3002;
+
+// Database configuration
+var dbConfig = { 
+  server: "localhost",   
+  database: 'Order',
+  driver: 'msnodesqlv8',
+  options: {
+    trustedConnection: true, // Use Windows authentication       
+  },
+};
+
+// Establish a connection pool
+sql.connect(dbConfig)
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Database connection failed', err));
+
+ 
 
 app.listen(PORT, () => {
   console.log(`MicroserviceB listening on port ${PORT}`);
