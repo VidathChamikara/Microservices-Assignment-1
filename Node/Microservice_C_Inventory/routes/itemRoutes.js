e029c107ecbcbd5553415aa32347a2f7ad1b43f7
Githmi Paranahewa <githmiph.20@itfac.mrt.ac.lk>
2023-08-21 19:59:35 +0530

    Do some minor changes

--- Node/Microservice_C_Inventory/routes/itemRoutes.js
+++ Node/Microservice_C_Inventory/routes/itemRoutes.js
@@ -2,62 +2,13 @@ const express = require("express");
 const itemController = require('../controller/item-controller');
 
 const router = express.Router();
-router.post('/addItem',(req,res) => {
-    Item.name = req.body.name;
-    Item.type = req.body.type;
-    Item.price = req.body.price;
-    Item.quantity = req.body.quantity;
-    Item.enteredBy = req.body.enteredBy;
 
+router.post('/addItem', itemController.addItem);
+router.get('/getAllItems', itemController.getAllItems);
+router.get('/getItem/:id', itemController.getItem);
+router.put('/updateItem/:id', itemController.updateItem);
+router.delete('/deleteItem/:id', itemController.deleteItem);
 
-    if(Item.name && Item.type && Item.price && Item.quantity && Item.enteredBy){
-        itemController.addItem(Item,(result) => {  
-            res.send({status:200,message:"Item added successfully"});     
-    });
-    }
-    else{
-        res.send({status:400,message:"Invalid data"});
-    }
-    
-}) ;
-
-router.get('/getAllItems',(req,res) => {
-    itemController.getAllItems((result) => {
-        res.send({status:200,data:result});
-    });
-  });
-
-  router.get('/getItem',(req,res) => {
-    const id = req.query.id;
-    itemController.getItem(id,(result) => {
-        res.send({status:200,data:result});
-    });
-  });
-
-  router.put('/updateItem',(req,res) => {
-    const id = req.body.iditem;
-    Item.name = req.body.name;
-    Item.type = req.body.type;
-    Item.price = req.body.price;
-    Item.quantity = req.body.quantity;
-    Item.enteredBy = req.body.enteredBy;
-
-    if(Item.name && Item.type && Item.price && Item.quantity && Item.enteredBy){
-        itemController.updateItem(id,Item,(result) => {
-            res.send({status:200,data:result});
-        });
-    }else{
-        res.send({status:400,message:"Invalid data"});
-    }
-    
-  });
-
-  router.delete('/deleteItem',(req,res) => {
-    const id = req.query.id;
-    itemController.deleteItem(id,(result) => {
-        res.send({status:200,data:result});
-    });
-  });
 
 module.exports = router;
 
