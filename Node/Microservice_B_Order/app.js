1261290f3760253a31face6db8f30f182b0332cf
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-08-21 16:43:10 +0530

    Update some files

--- Node/Microservice_B_Order/app.js
+++ Node/Microservice_B_Order/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
-
+const dbConnection = require('./dbConnection'); // Path to dbConnection.js
+const orderRoute = require('./route/OrderRoute'); // Path to OrderRoute.js
+const orderItemRoute = require('./route/OrderItemRoute'); // Path to OrderItemRoute.js
 
 const app = express();
 
 const PORT = 3002;
+
+// Establish database connection
+dbConnection.connectToDatabase();
+
+// Add middleware to parse JSON requests
+app.use(express.json());
+
+// Use order routes
+app.use('/api', orderRoute);
+app.use('/api', orderItemRoute);
 
 app.listen(PORT, () => {
   console.log(`MicroserviceB listening on port ${PORT}`);
