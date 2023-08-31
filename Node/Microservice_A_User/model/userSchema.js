9a939fe86bdb2cf33511fcde5270dfca4fc2fd0e
shiran-dinushka <shirandinushka9@gmail.com>
2023-08-31 12:31:50 +0530

    Implement validation

--- Node/Microservice_A_User/model/userSchema.js
+++ Node/Microservice_A_User/model/userSchema.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 const UserSchema = new mongoose.Schema({
     id:{type: String, require: true},
     name:{type: String, require: true},
     role:{type: String, require: true},
+    email:{type: String, require: true, unique: true,match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/ },
     password:{type: String, require: true},
     address:{type: String, require: true},
+});
+
+UserSchema.index({ name: 1, password: 1 }, { unique: true });
+UserSchema.plugin(uniqueValidator,{
+    message: 'Error, {VALUE} is already exists.'
 });
 
 module.exports = mongoose.model('User', UserSchema);
